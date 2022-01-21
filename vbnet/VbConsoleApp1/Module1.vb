d9cf76261d31e94c0ee939554b8205417179cc78
Andrea Cattaneo <andrea.cattaneo@infoesse.it>
2022-01-21 15:23:43 +0100

    sixth

---
+++ vbnet/VbConsoleApp1/Module1.vb
@@ -0,0 +1,7 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject("hello"""))
+    End Sub
+
+End Module
---
+++ vbnet/VbConsoleApp1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Controllare i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("VbConsoleApp1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VbConsoleApp1")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà usato come ID del typelib
+<Assembly: Guid("31e88b54-75b2-477c-a1c0-926be6771112")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("9.8.*")>
+<Assembly: AssemblyFileVersion("111.222.333.9")>
